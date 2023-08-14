d076d768da6179a190c8bf64200a46acd5d8d71d
Jame Chaney <macneilslt@gmail.com>
2023-08-14 19:21:28 -0400

    added extra reducers and fixed typos

--- src/features/coordinates/coordinatesSlice.js
+++ src/features/coordinates/coordinatesSlice.js
@@ -1,22 +1,47 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const API_KEY = process.env.REACT_APP_GMAP_API;
-//const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${API_KEY}`;
 
 export const fetchCoordinates = createAsyncThunk('coordinates/fetchCoordinates', async (zipcode) => {
-	const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${API_KEY}`);
-	if (!response.ok) {
-		return Promise.reject('Unable to fetch Latitude/Longitude: ' + response.status);
+	try {
+		const response = await fetch(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${API_KEY}`
+		);
+		if (!response.ok) {
+			throw new Error('Unable to fetch Latitude/Longitude');
+		}
+		const data = await response.json();
+		return data.results[0].geometry.location;
+	} catch (error) {
+		throw error;
 	}
-	const data = await response.json();
-	return data;
 });
+
+const initialState = {
+	latitude: null,
+	longitude: null,
+	isLoading: true,
+	errMsg: '',
+};
 
 export const coordinatesSlice = createSlice({
 	name: 'coordinates',
-	initialState: {
-		latitude: '',
-		longitude: '',
+	initialState,
+	reducers: {},
+	extraReducers: {
+		[fetchCoordinates.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[fetchCoordinates.fulfilled]: (state, action) => {
+			state.isLoading = false;
+			state.errMsg = '';
+			state.latitude = action.payload.lat;
+			state.longitude = action.payload.lng;
+		},
+		[fetchCoordinates.rejected]: (state, action) => {
+			state.isLoading = false;
+			state.errMsg = action.error ? action.error.message : 'Fetch failed';
+		},
 	},
 });
 
