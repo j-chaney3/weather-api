31b28bf1e9c4ab3073c7a1994c43a0b9cd96a162
Jame Chaney <macneilslt@gmail.com>
2023-08-16 18:24:17 -0400

    added city and state data from NWSFetchSlice

--- src/components/ForecastFetch.js
+++ src/components/ForecastFetch.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchNWSPoints } from '../features/nwsFetch/nwsFetchSlice';
 
 const ForecastFetch = () => {
-	// Access latitude and longitude from the coordinates state
+	const dispatch = useDispatch();
 	const { latitude, longitude } = useSelector((state) => state.coordinates);
+	const { city, state } = useSelector((state) => state.NWSPoints);
+
+	useEffect(() => {
+		if (latitude && longitude) {
+			const cString = latitude.toFixed(4) + ',' + longitude.toFixed(4);
+			console.log('Truncated:' + cString);
+			dispatch(fetchNWSPoints(cString));
+		}
+	}, [dispatch, latitude, longitude]);
 
 	if (!latitude && !longitude) {
-		return <div>Please entere your zipcode to see the current forcest.</div>;
+		return <div>Please enter your zipcode to see the current forecast.</div>;
 	}
 
 	return (
 		<div>
-			<h1>I'll use these for the forecast</h1>
+			<h1>
+				{city}, {state}
+			</h1>
 			<p>Latitude: {latitude}</p>
 			<p>Longitude: {longitude}</p>
 		</div>
