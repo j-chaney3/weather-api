bee8ea29bf220059181cea69d607b4b60ba177f6
Jame Chaney <macneilslt@gmail.com>
2023-08-16 18:33:50 -0400

    aded NWS points data to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { coordinatesReducer } from '../features/coordinates/coordinatesSlice';
+import { NWSReducer } from '../features/nwsFetch/nwsFetchSlice';
 
 export const store = configureStore({
 	reducer: {
 		coordinates: coordinatesReducer,
+		NWSPoints: NWSReducer,
 	},
 });
