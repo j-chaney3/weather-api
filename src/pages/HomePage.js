9f1dca61510cc09eacc2f6824752430dd22d84d0
Jame Chaney <macneilslt@gmail.com>
2023-08-28 16:14:39 -0400

    styling changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,12 @@ import ForecastFetch from '../components/ForecastFetch';
 
 const HomePage = () => {
 	return (
-		<div className="bg-slate-600 h-screen">
+		<div className="bg-slate-600 min-h-screen">
 			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 container p-2 mx-auto">
-				<div className="bg-slate-300 ">
+				<div className="bg-slate-300">
 					<CoordinatesConverter />
 				</div>
-				<div className="bg-slate-300 ">
+				<div className="bg-slate-300">
 					<ForecastFetch />
 				</div>
 			</div>
