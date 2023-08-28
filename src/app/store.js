07e1872925c40d91da2df8bd4a7018f0961a711f
Jame Chaney <macneilslt@gmail.com>
2023-08-28 16:09:21 -0400

    added forecastHourly reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { coordinatesReducer } from '../features/coordinates/coordinatesSlice';
 import { NWSReducer } from '../features/nwsFetch/nwsFetchSlice';
+import { forecastHourlyReducer } from '../features/nwsFetch/forecastHourlyFetchSlice';
 
 export const store = configureStore({
 	reducer: {
 		coordinates: coordinatesReducer,
 		NWSPoints: NWSReducer,
+		forecastHourly: forecastHourlyReducer,
 	},
 });
