239c9f356ccb4346d06837b8b734eaca6af5bb68
Jame Chaney <macneilslt@gmail.com>
2023-08-28 16:10:18 -0400

    added hourly forecast to component

--- src/components/ForecastFetch.js
+++ src/components/ForecastFetch.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchNWSPoints } from '../features/nwsFetch/nwsFetchSlice';
+import { fetchForecastHourly, select24hours } from '../features/nwsFetch/forecastHourlyFetchSlice';
 
 const ForecastFetch = () => {
+	const [forecastType, setForecastType] = useState('hourly');
 	const dispatch = useDispatch();
 	const { latitude, longitude } = useSelector((state) => state.coordinates);
-	const { city, state } = useSelector((state) => state.NWSPoints);
+	const { city, state, gridX, gridY, gridId } = useSelector((state) => state.NWSPoints);
 
 	useEffect(() => {
 		if (latitude && longitude) {
@@ -15,19 +17,113 @@ const ForecastFetch = () => {
 		}
 	}, [dispatch, latitude, longitude]);
 
+	useEffect(() => {
+		if (gridX && gridY && gridId && forecastType === 'hourly') {
+			const url = `https://api.weather.gov/gridpoints/${gridId}/${gridX},${gridY}/forecast/hourly`;
+			dispatch(fetchForecastHourly(url));
+		}
+	}, [dispatch, gridX, gridY, gridId]);
+
+	//map 24 periods
+	const hourly = useSelector(select24hours);
+
+	//date time format conversion
+	const formatTime = (isoTime) => {
+		const date = new Date(isoTime);
+		const options = {
+			hour: 'numeric',
+			minute: 'numeric',
+			hour12: true,
+		};
+
+		return date.toLocaleTimeString(undefined, options);
+	};
+
 	if (!latitude && !longitude) {
 		return <div>Please enter your zipcode to see the current forecast.</div>;
 	}
 
-	return (
-		<div>
-			<h1>
-				{city}, {state}
-			</h1>
-			<p>Latitude: {latitude}</p>
-			<p>Longitude: {longitude}</p>
-		</div>
-	);
+	//show hourly
+	if (forecastType === 'hourly') {
+		return (
+			<div>
+				<h1>
+					{city}, {state}
+				</h1>
+				<p>Latitude: {latitude}</p>
+				<p>Longitude: {longitude}</p>
+				<br />
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1 my-4"
+					onClick={() => {
+						setForecastType('hourly');
+					}}
+				>
+					Hourly
+				</button>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  my-4"
+					onClick={() => {
+						setForecastType('daily');
+					}}
+				>
+					Daily
+				</button>
+
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-3">
+					{hourly.map((period, index) => (
+						<div key={index} className="flex items-center">
+							<div className="container mx-auto px-20 mb-5 w-9/10">
+								<div className="bg-white rounded-lg shadow-md p-6 h-64 flex flex-col justify-between">
+									<div className="w-16 mx-auto relative -mt-10 mb-3">
+										<img
+											className="-mt-1 rounded-full border-slate-400 border-2"
+											src={period.icon}
+											alt="weather icon"
+										/>
+									</div>
+									<p className="w-full sm:w-48 block leading-normal text-gray-800 text-md mb-3 text-s text-left mb-0 ">
+										Forecast: {period.shortForecast}
+									</p>
+									<p className="w-full sm:w-48 block leading-normal text-gray-800 text-md mb-3 text-s text-left">
+										Chance of Precipitation: {period.probabilityOfPrecipitation.value}% <br />
+										Temperature: {period.temperature}F
+									</p>
+									<div className="mx-auto relative text-center">
+										<p className="text-xs text-gray-400 mr-1 text-center">
+											{formatTime(period.startTime)} to {formatTime(period.endTime)}
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<h1>Daily Forecast</h1>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1 my-4"
+					onClick={() => {
+						setForecastType('hourly');
+					}}
+				>
+					Hourly
+				</button>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  my-4"
+					onClick={() => {
+						setForecastType('daily');
+					}}
+				>
+					Daily
+				</button>
+			</div>
+		);
+	}
 };
 
 export default ForecastFetch;
