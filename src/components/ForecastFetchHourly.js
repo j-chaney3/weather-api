f71fb4eaae5925b80adc04bb3838205a085fb280
Jame Chaney <macneilslt@gmail.com>
2023-09-13 09:57:53 -0400

    styling changes to align with daily forecast

--- src/components/ForecastFetchHourly.js
+++ src/components/ForecastFetchHourly.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchNWSPoints } from '../features/nwsFetch/nwsFetchSlice';
 import { fetchForecastHourly, select24hours, selectTemps } from '../features/nwsFetch/forecastHourlyFetchSlice';
@@ -9,7 +9,6 @@ import { formatTime } from '../utilities/dateTimeFormat';
 import { urlSubstring } from '../utilities/urlSubString';
 
 const ForecastFetchHourly = () => {
-	const [forecastType, setForecastType] = useState('hourly');
 	const dispatch = useDispatch();
 	const { latitude, longitude } = useSelector((state) => state.coordinates);
 	const { city, state, gridX, gridY, gridId, isLoading, errMsg } = useSelector((state) => state.NWSPoints);
@@ -32,6 +31,7 @@ const ForecastFetchHourly = () => {
 
 	const hourly = useSelector(select24hours);
 	const tempArray = useSelector(selectTemps);
+	const { low, high } = lowHigh(tempArray);
 
 	if (!latitude && !longitude) {
 		return <div>Please enter your zipcode to see the current forecast.</div>;
@@ -45,79 +45,54 @@ const ForecastFetchHourly = () => {
 		);
 	}
 
-	if (forecastType === 'hourly' && !isLoading) {
-		const { low, high } = lowHigh(tempArray);
+	return (
+		<div>
+			<h1 className="font-bold">Hourly Forecast</h1>
+			<h1>
+				{city}, {state}
+			</h1>
+			<p>Latitude: {latitude}</p>
+			<p>Longitude: {longitude}</p>
 
-		return (
-			<div>
-				<h1>
-					{city}, {state}
-				</h1>
-				<p>Latitude: {latitude}</p>
-				<p>Longitude: {longitude}</p>
-
-				<br />
-
-				<div className="mb-5">
-					Low: {low} °F / High: {high} °F
-				</div>
-				<div className="grid grid-cols-1 gap-3">
-					{hourly.map((period, index) => (
-						<div key={index} className="flex items-center">
-							<div className="container mx-auto px-10 mb-5">
-								<div className="bg-white rounded-lg shadow-md p-6 h-auto w-full flex flex-col justify-between">
-									<div className="photo-wrapper p-2 w-a mx-auto relative mt-10 mb-3">
-										<img
-											className="w-225 h-225 rounded-full mx-auto"
-											src={urlSubstring(period.icon)}
-											alt="forecast icon"
-										/>
+			<div className="m-3">
+				Today's Temperatures - Low: {low} °F / High: {high} °F
+			</div>
+			<div className="grid grid-cols-1 gap-3">
+				{hourly.map((period, index) => (
+					<div key={index} className="flex items-center">
+						<div className="container mx-auto px-10 mb-5">
+							<div className="bg-white rounded-lg shadow-md p-6 h-auto w-full flex flex-col justify-between">
+								<div className="photo-wrapper p-2 w-a mx-auto relative mt-10 mb-3">
+									<img
+										className="w-225 h-225 rounded-full mx-auto"
+										src={urlSubstring(period.icon)}
+										alt="forecast icon"
+									/>
+								</div>
+								<div className="p-2">
+									<h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+										{period.temperature}°F
+									</h3>
+									<div className="text-center text-gray-black text-xs font-semibold">
+										<p>Precipitation: {period.probabilityOfPrecipitation.value}%</p>
 									</div>
-									<div className="p-2">
-										<h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-											{period.temperature}°F
-										</h3>
-										<div className="text-center text-gray-black text-xs font-semibold">
-											<p>Precipitation: {period.probabilityOfPrecipitation.value}%</p>
-										</div>
-										<div className="text-center text-black-400 text-xs font-semibold">
-											<p>Forecast: {period.shortForecast}</p>
-										</div>
+									<div className="text-center text-black-400 text-xs font-semibold">
+										<p>
+											<span className="font-bold">Forecast:</span> {period.shortForecast}
+										</p>
+									</div>
 
-										<div className="text-center text-gray-400 text-xs font-semibold my-3 mx-auto relative">
-											<p>{formatTime(period.startTime)}</p>
-										</div>
+									<div className="text-center text-gray-400 text-xs font-semibold my-3 mx-auto relative">
+										<p>{formatTime(period.startTime)}</p>
 									</div>
 								</div>
 							</div>
 						</div>
-					))}
-				</div>
+					</div>
+				))}
 			</div>
-		);
-	} else {
-		return (
-			<div>
-				<h1>Daily Forecast</h1>
-				<button
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1 my-4"
-					onClick={() => {
-						setForecastType('hourly');
-					}}
-				>
-					Hourly
-				</button>
-				<button
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  my-4"
-					onClick={() => {
-						setForecastType('daily');
-					}}
-				>
-					Daily
-				</button>
-			</div>
-		);
-	}
+		</div>
+	);
 };
 
 export default ForecastFetchHourly;
