009b728d9b820df27f8683a7c98ba923db381879
Jame Chaney <macneilslt@gmail.com>
2023-09-13 07:36:21 -0400

    added daily forecast reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { coordinatesReducer } from '../features/coordinates/coordinatesSlice';
 import { NWSReducer } from '../features/nwsFetch/nwsFetchSlice';
 import { forecastHourlyReducer } from '../features/nwsFetch/forecastHourlyFetchSlice';
+import { forecastDailyReducer } from '../features/nwsFetch/forecastDailyFetchSlice';
 
 export const store = configureStore({
 	reducer: {
 		coordinates: coordinatesReducer,
 		NWSPoints: NWSReducer,
 		forecastHourly: forecastHourlyReducer,
+		forecastDaily: forecastDailyReducer,
 	},
 });
