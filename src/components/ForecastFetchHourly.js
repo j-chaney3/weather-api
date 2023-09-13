f8e5fc43ea10fc71534410b04a4033ec7a30767f
Jame Chaney <macneilslt@gmail.com>
2023-09-13 08:11:26 -0400

    minor formatting adjustments

--- src/components/ForecastFetchHourly.js
+++ src/components/ForecastFetchHourly.js
@@ -6,6 +6,7 @@ import { fetchForecastHourly, select24hours, selectTemps } from '../features/nws
 //imported functions
 import { lowHigh } from '../utilities/lowHigh';
 import { formatTime } from '../utilities/dateTimeFormat';
+import { urlSubstring } from '../utilities/urlSubString';
 
 const ForecastFetchHourly = () => {
 	const [forecastType, setForecastType] = useState('hourly');
@@ -58,7 +59,7 @@ const ForecastFetchHourly = () => {
 				<br />
 
 				<div className="mb-5">
-					Low: {low} / High: {high}
+					Low: {low} °F / High: {high} °F
 				</div>
 				<div className="grid grid-cols-1 gap-3">
 					{hourly.map((period, index) => (
@@ -67,8 +68,8 @@ const ForecastFetchHourly = () => {
 								<div className="bg-white rounded-lg shadow-md p-6 h-auto w-full flex flex-col justify-between">
 									<div className="photo-wrapper p-2 w-a mx-auto relative mt-10 mb-3">
 										<img
-											className="w-200 h-200 rounded-full mx-auto"
-											src={period.icon}
+											className="w-225 h-225 rounded-full mx-auto"
+											src={urlSubstring(period.icon)}
 											alt="forecast icon"
 										/>
 									</div>
