87d28128eca5ceebaf120fc2d9b66e9e7b826f37
Jame Chaney <macneilslt@gmail.com>
2023-09-13 07:39:44 -0400

    styling & layout changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,17 @@
 import CoordinatesConverter from '../components/CoordinatesConverter';
-import ForecastFetch from '../components/ForecastFetch';
+import ForecastFetchHourly from '../components/ForecastFetchHourly';
 
 const HomePage = () => {
 	return (
-		<div className="bg-slate-600 min-h-screen">
-			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 container p-2 mx-auto">
-				<div className="bg-slate-300">
-					<CoordinatesConverter />
-				</div>
-				<div className="bg-slate-300">
-					<ForecastFetch />
+		<div className="bg-slate-500">
+			<div className="bg-slate-600 min-h-screen max-w-screen-md mx-auto bg-slate-300">
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-1 container p-2 mx-auto">
+					<div className="bg-slate-300">
+						<CoordinatesConverter />
+					</div>
+					<div className="bg-slate-300">
+						<ForecastFetchHourly />
+					</div>
 				</div>
 			</div>
 		</div>
