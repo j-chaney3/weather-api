33bc6679d5a3326ed8aa3a5ebec753c2369a9b7d
Jame Chaney <macneilslt@gmail.com>
2023-09-14 01:51:37 -0400

    added basic validation

--- src/components/CoordinatesConverter.js
+++ src/components/CoordinatesConverter.js
@@ -7,6 +7,7 @@ const CoordinatesConverter = () => {
 	//const { latitude, longitude } = useSelector((state) => state.coordinates);
 
 	const [zipcode, setZipcode] = useState('');
+	const [isValidZipcode, setIsValidZipcode] = useState(false);
 
 	const handleConvertClick = async () => {
 		try {
@@ -16,17 +17,36 @@ const CoordinatesConverter = () => {
 		}
 	};
 
+	const validate = (value) => {
+		const regex = /^\d{5}$/;
+
+		if (regex.test(value)) {
+			setIsValidZipcode(true);
+		} else {
+			setIsValidZipcode(false);
+		}
+	};
+
 	return (
 		<div>
 			<input
 				type="text"
 				placeholder="Enter zipcode"
 				value={zipcode}
-				onChange={(event) => setZipcode(event.target.value)}
+				// onChange={
+				// 	(event) => setZipcode(event.target.value)
+				// }
+				onChange={(event) => {
+					setZipcode(event.target.value);
+					validate(event.target.value);
+				}}
+				maxLength={5}
 			/>
 			<button
+				id="check"
 				onClick={handleConvertClick}
-				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-4 my-4"
+				className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-4 my-4 disabled:bg-red-200`}
+				disabled={!isValidZipcode}
 			>
 				Check Weather!
 			</button>
