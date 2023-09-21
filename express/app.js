c8327f8afb30027a525ff37b4e0c4ce8024bf7ad
Jame Chaney <macneilslt@gmail.com>
2023-09-20 21:20:51 -0400

    using .get/.then/.catch instead of try/catch

--- express/app.js
+++ express/app.js
@@ -20,26 +20,26 @@ const port = 3000;
 
 app.get('/geocode/:zipcode', async (req, res) => {
 	const { zipcode } = req.params;
-
-	try {
-		const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${GOOGLE_API_KEY}`;
-		const response = await axios.get(geocodingUrl);
-
-		if (response.status === 200) {
-			const data = response.data;
-			if (data.status === 'OK') {
-				const location = data.results[0].geometry.location;
-				res.json(location);
+	const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${GOOGLE_API_KEY}`;
+	axios
+		.get(geocodingUrl)
+		.then((response) => {
+			if (response.status === 200) {
+				const data = response.data;
+				if (data.status === 'OK') {
+					const location = data.results[0].geometry.location;
+					res.json(location);
+				} else {
+					res.status(400).json({ error: 'Invalid Zipcode' });
+				}
 			} else {
-				res.status(400).json({ error: 'Geocoding request failed' });
+				res.status(500).json({ error: 'request failed' });
 			}
-		} else {
-			res.status(500).json({ error: 'Geocoding request failed' });
-		}
-	} catch (error) {
-		console.error('Error calling the Geocoding API:', error.message);
-		res.status(500).json({ error: 'An error occurred while calling the Geocoding API' });
-	}
+		})
+		.catch((error) => {
+			console.error('Error calling google geocoding API:', error.message);
+			res.status(500).json({ error: 'An error occured with the google geocoordinates API' });
+		});
 });
 
 app.listen(port, () => {
