747d25f130bb9b669a9609636da3b5b4d0780b2b
Jame Chaney <macneilslt@gmail.com>
2023-09-20 21:19:30 -0400

    changed async thunk to call fetch from express instead of directly calling api

--- src/features/coordinates/coordinatesSlice.js
+++ src/features/coordinates/coordinatesSlice.js
@@ -1,18 +1,13 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-//const API_KEY = process.env.REACT_APP_GMAP_API;
-
 export const fetchCoordinates = createAsyncThunk('coordinates/fetchCoordinates', async (zipcode) => {
 	try {
-		const response = await fetch(
-			//`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${API_KEY}`
-			`http://localhost:3000/geocode/:${zipcode}`
-		);
+		const response = await fetch(`http://localhost:3000/geocode/:${zipcode}`);
 		if (!response.ok) {
-			throw new Error('Unable to fetch Latitude/Longitude');
+			throw new Error('Invalid Zipcode');
 		}
 		const data = await response.json();
-		//return data.results[0].geometry.location;
+
 		return data;
 	} catch (error) {
 		throw error;
