a1bb1fe8b60da5f1f9b4c6f3381609add164a217
Jame Chaney <macneilslt@gmail.com>
2023-09-29 08:17:18 -0400

    change: minor change in color for hover/active buttons

--- src/components/HourlyDailyButtons.js
+++ src/components/HourlyDailyButtons.js
@@ -6,8 +6,8 @@ const HourlyDailyButtons = ({ hourly, setHourly }) => {
 					setHourly(true);
 				}}
 				className={`${
-					hourly ? 'bg-green-500' : 'bg-blue-500'
-				} hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1 my-1 w-32`}
+					hourly ? 'bg-blue-800' : 'bg-blue-500'
+				} hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-1 my-1 w-32`}
 			>
 				Hourly
 			</button>
@@ -16,8 +16,8 @@ const HourlyDailyButtons = ({ hourly, setHourly }) => {
 					setHourly(false);
 				}}
 				className={`${
-					!hourly ? 'bg-green-500' : 'bg-blue-500'
-				} hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1 my-1 w-32`}
+					!hourly ? 'bg-blue-800' : 'bg-blue-500'
+				} hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-1 my-1 w-32`}
 			>
 				Daily
 			</button>
