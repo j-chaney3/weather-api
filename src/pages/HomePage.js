18aa1b381e984c1e2775237609079b85d4e8a811
Jame Chaney <macneilslt@gmail.com>
2023-09-29 07:47:04 -0400

    update: removed Y margin from container div

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
 
 	return (
 		<div className="bg-slate-500">
-			<div className="bg-slate-600 min-h-screen max-w-screen-md mx-auto">
+			<div className="bg-slate-300 min-h-screen max-w-screen-md mx-auto">
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-1 container p-2 mx-auto">
 					<div className="bg-slate-300">
 						<CoordinatesConverter hourly={hourly} setHourly={setHourly} />
