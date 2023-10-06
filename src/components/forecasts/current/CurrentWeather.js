2c7a80edb4a376119a5289dfcef069d58c764763
Jame Chaney <macneilslt@gmail.com>
2023-10-06 15:56:36 -0400

    update: added current temperature

--- src/components/forecasts/current/CurrentWeather.js
+++ src/components/forecasts/current/CurrentWeather.js
@@ -1,18 +1,30 @@
 import { formatDate, formatTime } from '../../../utilities/dateTimeFormat';
+import { selectCurrent } from '../../../features/nwsFetch/forecastHourlyFetchSlice';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 
 const CurrentWeather = ({ city, state, zipcode, updated, latitude, longitude, high, low }) => {
+	const current = useSelector(selectCurrent);
+
 	return (
 		<div>
 			<h1 className="font-bold">
 				{city}, {state} {zipcode ? ` - ${zipcode}` : ''}
 			</h1>
-			<h2 className="font-semibold">
-				Updated at: {formatDate(updated)}, {formatTime(updated)}
-			</h2>
-			<p>Latitude: {latitude || 'N/A'}</p>
-			<p>Longitude: {longitude || 'N/A'}</p>
-			<div className="m-3 text-md">
-				Today's Temperatures - Low: {low} °F / High: {high} °F
+			<div className="inline-block">
+				<p className="italic text-sm">
+					{formatDate(updated)}, {formatTime(updated)}
+				</p>
+			</div>
+			<div>
+				<p>Latitude: {latitude || 'N/A'}</p>
+				<p>Longitude: {longitude || 'N/A'}</p>
+			</div>
+
+			<div className="m-1 text-md">
+				<p>
+					Low: {low} °F / High: {high} °F
+				</p>
+				<p>Current Temperature: {current.temperature}°F </p>
 			</div>
 		</div>
 	);
