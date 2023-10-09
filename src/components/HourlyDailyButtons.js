5dba71201abce4290266efc30adffef1489a0f83
Jame Chaney <macneilslt@gmail.com>
2023-10-09 09:11:06 -0400

    update: updated to use redux managed state, removed prop drilling

--- src/components/HourlyDailyButtons.js
+++ src/components/HourlyDailyButtons.js
@@ -1,22 +1,28 @@
-const HourlyDailyButtons = ({ hourly, setHourly }) => {
+import { useDispatch, useSelector } from 'react-redux/';
+import { setDailyForecast, setHourlyForecast } from '../features/forecastType/forecastTypeSlice';
+
+const HourlyDailyButtons = () => {
+	const dispatch = useDispatch();
+	const { forecastType } = useSelector((state) => state.setForecastType);
+
 	return (
 		<div className="justify-items-center">
 			<button
 				onClick={() => {
-					setHourly(true);
+					dispatch(setHourlyForecast());
 				}}
 				className={`${
-					hourly ? 'bg-blue-800' : 'bg-blue-500'
+					forecastType === 'hourly' ? 'bg-blue-800' : 'bg-blue-500'
 				} hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-1 my-1 w-32 ju`}
 			>
 				Hourly
 			</button>
 			<button
 				onClick={() => {
-					setHourly(false);
+					dispatch(setDailyForecast());
 				}}
 				className={`${
-					!hourly ? 'bg-blue-800' : 'bg-blue-500'
+					forecastType === 'daily' ? 'bg-blue-800' : 'bg-blue-500'
 				} hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-1 my-1 w-32`}
 			>
 				Daily
