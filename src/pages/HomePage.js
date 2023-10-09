8743d73da88ef545cb3e9d12e4d07e8207657b2f
Jame Chaney <macneilslt@gmail.com>
2023-10-09 09:16:12 -0400

    update: removed props, added switch to mange forecast type

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import CoordinatesConverter from '../components/coordinates/CoordinatesConverter';
 import DailyForecast from '../components/forecasts/daily/DailyForecast';
@@ -6,20 +5,33 @@ import HourlyForecast from '../components/forecasts/hourly/HourlyForecast';
 import HourlyDailyButtons from '../components/HourlyDailyButtons';
 
 const HomePage = () => {
-	const [hourly, setHourly] = useState(true);
 	const { err } = useSelector((state) => state.coordinates);
+	const { forecastType } = useSelector((state) => state.setForecastType);
+
+	let selectedForecast;
+	switch (forecastType) {
+		case 'hourly':
+			selectedForecast = <HourlyForecast />;
+			break;
+		case 'daily':
+			selectedForecast = <DailyForecast />;
+			break;
+		default:
+			selectedForecast = <HourlyForecast />;
+			break;
+	}
 
 	return (
 		<div className="bg-slate-500">
 			<div className="bg-slate-300 min-h-screen max-w-screen-md mx-auto">
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-1 container p-2 mx-auto">
 					<div className="bg-slate-300">
-						<CoordinatesConverter hourly={hourly} setHourly={setHourly} />
+						<CoordinatesConverter />
 					</div>
 
-					<div className="bg-slate-300">
-						{!err && <HourlyDailyButtons hourly={hourly} setHourly={setHourly} />}
-						{hourly ? <HourlyForecast /> : <DailyForecast />}
+					<div className="bg slate-300">
+						{!err && <HourlyDailyButtons />}
+						{selectedForecast}
 					</div>
 				</div>
 			</div>
