4bf1d8453a7d4bf21af09b1bec0fa61c22b4b2a2
Jame Chaney <macneilslt@gmail.com>
2023-12-26 01:54:29 -0500

    add check to verify successful fetch

--- src/components/forecasts/current/CurrentWeather.js
+++ src/components/forecasts/current/CurrentWeather.js
@@ -19,29 +19,49 @@ const CurrentWeather = ({ city, state, zipcode, updated, latitude, longitude, hi
 			break;
 	}
 
-	return (
-		<div>
-			<h1 className="font-bold">
-				{city}, {state} | {selectedForecast}
-			</h1>
-			<div className="inline-block">
-				<p className="italic text-sm">
-					Updated at: {formatDate(updated)}, {formatTime(updated)}
-				</p>
-			</div>
+	//ensure current data fetched successfully before return
+	if (current !== undefined) {
+		return (
 			<div>
-				<p>Latitude: {latitude || 'N/A'}</p>
-				<p>Longitude: {longitude || 'N/A'}</p>
-			</div>
+				<h1 className="font-bold">
+					{city}, {state} | {selectedForecast}
+				</h1>
+				<div className="inline-block">
+					<p className="italic text-sm">
+						Updated at: {formatDate(updated)}, {formatTime(updated)}
+					</p>
+				</div>
+				<div>
+					<p>Latitude: {latitude || 'N/A'}</p>
+					<p>Longitude: {longitude || 'N/A'}</p>
+				</div>
 
-			<div className="m-1 text-md">
-				<p>
-					Low: {low} °F / High: {high} °F
-				</p>
-				<p>Current Temperature: {current.temperature}°F </p>
+				<div className="m-1 text-md">
+					<p>
+						Low: {low} °F / High: {high} °F
+					</p>
+					<p>Current Temperature: {current.temperature}°F </p>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div>
+				<p>
+					The
+					<a
+						className="px-1 font-medium text-blue-600 dark:text-blue-500 hover:underline"
+						href="https://github.com/weather-gov/api/discussions/688"
+						target="_blank"
+						rel="noreferrer"
+					>
+						weather.gov
+					</a>
+					api used for this app is down until Jan, 1st, 2024. Sorry for the inconvenience.
+				</p>
+			</div>
+		);
+	}
 };
 
 export default CurrentWeather;
