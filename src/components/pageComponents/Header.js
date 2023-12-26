9af8548c946ed38ed3f0f9fdfadc8a9c25e0a0e8
Jame Chaney <macneilslt@gmail.com>
2023-12-26 01:53:16 -0500

    added link rels

--- src/components/pageComponents/Header.js
+++ src/components/pageComponents/Header.js
@@ -7,10 +7,7 @@ const SiteHeader = () => {
 			<nav className="CustomNav relative flex flex-wrap items-center justify-between bg-slate-700 mb-0">
 				<div className="container px-2 mx-auto flex flex-wrap items-center justify-between">
 					<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-						<p
-							className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-							href="#pablo"
-						>
+						<p className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
 							Weather Forecast
 						</p>
 						<button
@@ -22,7 +19,9 @@ const SiteHeader = () => {
 						</button>
 					</div>
 					<div
-						className={'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')}
+						className={
+							'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')
+						}
 						id="example-navbar-danger"
 					>
 						<ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
@@ -31,6 +30,7 @@ const SiteHeader = () => {
 									className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
 									href="https://www.linkedin.com/in/james-c-b3a05585/"
 									target="_blank"
+									rel="noreffer"
 								>
 									<i className="fab fa-linkedin text-lg leading-lg text-white opacity-75"></i>
 								</a>
@@ -40,6 +40,7 @@ const SiteHeader = () => {
 									className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
 									href="https://github.com/j-chaney3/weather-api"
 									target="_blank"
+									rel="noreffer"
 								>
 									<i className="fab fa-github text-lg leading-lg text-white opacity-75"></i>
 								</a>
